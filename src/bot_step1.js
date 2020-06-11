bb9c711430b1af9e5b8175955456473a19ccb876
Cxeen <66783211+Cxeen@users.noreply.github.com>
2020-06-11 16:17:24 +0300

    bruh
    bruh

--- src/bot_step1.js
+++ src/bot_step1.js
@@ -1,37 +1,37 @@
 const eris = require('eris');
-const { BOT_TOKEN } = require('../config.json');
+const {    NzIwNjIxMzkyODI3MjUyNzQ3.XuIsgw.rR4pqU6JqABGK65WF6WYSywC3Cs  } = require('../config.json');
 
 // Create a Client instance with our bot token.
-const bot = new eris.Client(BOT_TOKEN);
+const bot = new eris.Client(  NzIwNjIxMzkyODI3MjUyNzQ3.XuIsgw.rR4pqU6JqABGK65WF6WYSywC3Cs  );
 
 // When the bot is connected and ready, log to console.
-bot.on('ready', () => {
-  console.log('Connected and ready.');
+bot.on('hazir', () => {
+  console.log('Aga hazirim.');
 });
 
 // Every time a message is sent anywhere the bot is present,
 // this event will fire and we will check if the bot was mentioned.
 // If it was, the bot will attempt to respond with "Present".
-bot.on('messageCreate', async (msg) => {
+bot.on('bruh', async (msg) => {
   const botWasMentioned = msg.mentions.find(
     mentionedUser => mentionedUser.id === bot.user.id,
   );
 
   if (botWasMentioned) {
     try {
-      await msg.channel.createMessage('Present');
+      await msg.channel.createMessage('bbbruh');
     } catch (err) {
       // There are various reasons why sending a message may fail.
       // The API might time out or choke and return a 5xx status,
       // or the bot may not have permission to send the
       // message (403 status).
-      console.warn('Failed to respond to mention.');
+      console.warn('mal orosoxy evladi yanlis yazdin');
       console.warn(err);
     }
   }
 });
 
-bot.on('error', err => {
+bot.on('hata ulan', err => {
   console.warn(err);
 });
 
